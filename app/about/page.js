d84a7025a2edd9761cb3875962443a34fb705fef
Eskil K. Hagen <eskilkaspersen@gmail.com>
2024-09-02 23:37:02 +0200

    oikjnb

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ export default function About() {
         <div className="px-4 py-16 max-w-screen-xl flex flex-col gap-4 mx-auto">
             <h1 className="text-4xl font-bold">About Me</h1>
             <p className="text-lg opacity-80 leading-relaxed max-w-xl lg:max-w-2xl">
-                Hi, I'm Eskil K. Hagen, a passionate web developer and user
+                Hi, I&apos;m Eskil K. Hagen, a passionate web developer and user
                 interface designer. Since 2019, I&apos;ve been diving deep into
                 the world of technology, learning and implementing various tools
                 and frameworks to create functional and beautiful web
