0bb081355c10a105952f69a59a391da7ba3c79f1
Eskil K. Hagen <eskilkaspersen@gmail.com>
2024-09-02 23:32:17 +0200

    ``

--- app/about/page.js
+++ app/about/page.js
@@ -4,16 +4,17 @@ export default function About() {
             <h1 className="text-4xl font-bold">About Me</h1>
             <p className="text-lg opacity-80 leading-relaxed max-w-xl lg:max-w-2xl">
                 Hi, I'm Eskil K. Hagen, a passionate web developer and user
-                interface designer. Since 2019, I've been diving deep into the
-                world of technology, learning and implementing various tools and
-                frameworks to create functional and beautiful web applications.
+                interface designer. Since 2019, I&apos;ve been diving deep into
+                the world of technology, learning and implementing various tools
+                and frameworks to create functional and beautiful web
+                applications.
             </p>
             <p className="text-lg opacity-80 leading-relaxed max-w-xl lg:max-w-2xl">
                 My journey started with an interest in technology, and over the
-                years, I've honed my skills in fullstack development, focusing
-                on accessibility, user experience, and modern design principles.
-                I believe in the power of clean, simple, and efficient design to
-                enhance the user experience.
+                years, I&apos;ve honed my skills in fullstack development,
+                focusing on accessibility, user experience, and modern design
+                principles. I believe in the power of clean, simple, and
+                efficient design to enhance the user experience.
             </p>
         </div>
     );
